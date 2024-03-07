16bf0b2b95867490b2fc8e34e5f0c418db3b02a3
Wiktoria Skibińska <wikas2000@icloud.com>
2024-03-07 13:16:32 +0100

    filterSlice./setFilter completed

--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
 import css from './Filter.module.css';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { setFilter } from 'myredux/filterSlice';
 
-const Filter = ({ onFilterChange }) => {
+const Filter = () => {
+  const dispatch = useDispatch();
+  const onFilterChange = evt => {
+    evt.preventDefault();
+    const filterValue = evt.target.value;
+    dispatch(setFilter(filterValue.toLowerCase()));
+  };
+
+  /**  const onFilterChange = evt => {
+    setFilter(evt.target.value);
+  };
+
+  const getVisibleContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+   const visibleContacts = getVisibleContacts();
+  */
+
   return (
     <div className={css.filter}>
       <label className={css.filterLabel}>
